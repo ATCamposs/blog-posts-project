852d2bdb717732f85e6f48fdc1b99caf7a23d88e
ATCamposs <andre.tatibano@gmail.com>
2021-04-20 19:51:59 -0300

    Criados testes iniciais da entidade Post e adicionados métodos na mesma

--- app/Domain/Post.php
+++ app/Domain/Post.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace app\Domain;
 
 use app\Domain\ValueObjects\AuthorName;
+use app\Domain\ValueObjects\Exceptions\InvalidAuthorName;
+use app\Domain\ValueObjects\Exceptions\InvalidSlug;
 use app\Domain\ValueObjects\Slug;
+use app\Infrastructure\PostRepositoryIlluminate;
+use app\Infrastructure\UUIDGenerator;
 use DateTime;
-use Jenssegers\Mongodb\Eloquent\Model;
 
-class Post extends Model
+class Post
 {
-    use PostTrait;
-
     private string $uuid;
     private AuthorName $author_name;
     private Slug $slug;
@@ -42,8 +43,93 @@ class Post extends Model
         $this->updated = $updated;
     }
 
+    /** @return Array|Post */
+    public static function createNewPost(string $author_name, string $slug, string $image, string $content)
+    {
+        $uuid = UUIDGenerator::generate();
+        try {
+            $author_name = new AuthorName($author_name);
+            $slug = new Slug($slug);
+        } catch (InvalidAuthorName $error) {
+            return ['status' => 'fail', 'data' => ['authorName' => $error->getMessage()]
+            ];
+        } catch (InvalidSlug $error) {
+            return ['status' => 'fail', 'data' => ['slug' => $error->getMessage()]
+            ];
+        }
+        $now = new DateTime();
+
+        return new Post($uuid, $author_name, $slug, $image, $content, 0, $now, $now);
+    }
+
+    /** @return Array|Post */
+    public function getPostWithSlug(string $slug)
+    {
+        try {
+            $slug = new Slug($slug);
+        } catch (InvalidSlug $error) {
+            return ['status' => 'fail', 'data' => ['slug' => $error->getMessage()]
+            ];
+        }
+        $post = $this->getRepository()->getPostWithSlug($slug);
+        if (empty($post)) {
+            return [
+                'status' => 'fail',
+                'data' => ['post' => trans('Post saved successfully.')]
+            ];
+        }
+    }
+
     public function __get($name)
     {
         return $this->$name;
+    }
+
+    public function getRepository(): PostRepositoryInterface
+    {
+        return new PostRepositoryIlluminate();
+    }
+
+    public function savePost()
+    {
+        $existing_post_slug = $this->getRepository()->checkSlugExists($this->slug);
+        if ($existing_post_slug > 0) {
+            return [
+                'status' => 'fail',
+                'data' => ['slug' => trans('Error, there is already a post with this slug.')]
+            ];
+        }
+        if ($this->getRepository()->savePost($this)) {
+            return [
+                'status' => 'success',
+                'data' => ['post' => trans('Post saved successfully.')]
+            ];
+        }
+        return [
+            'status' => 'fail',
+            'data' => ['message' => trans('Error, please try again.')]
+        ];
+    }
+
+    public function updateAuthor(string $author_name)
+    {
+        try {
+            $author_name = new AuthorName($author_name);
+        } catch (InvalidAuthorName $error) {
+            return [
+                'status' => 'fail',
+                'data' => ['authorName' => $error->getMessage()]
+            ];
+        }
+        if ($this->getRepository()->saveAuthorNameUpdate($this->uuid, $author_name)) {
+            return [
+                'status' => 'success',
+                'data' => ['authorName' => trans('Author name updated successfully.')]
+            ];
+        }
+        return [
+            'status' => 'fail',
+            'data' => ['message' => trans('Error, please try again.')]
+        ];
     }
 }
---
+++ tests/Domain/PostTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace tests\Domain;
+
+use app\Domain\Post;
+use app\Domain\ValueObjects\AuthorName;
+use app\Domain\ValueObjects\Slug;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use support\Db;
+
+class PostTest extends TestCase
+{
+    public static Post $post;
+
+    public static string $uuid = 'uuidwillbegeneratedautomatically';
+    public static string $author_name = 'AndreCampos';
+    public static string $image = '/public/files/newphoto.jpg';
+    public static string $slug = 'create-a-post-for-do-the-tests';
+    public static string $content = 'This post will be created to run all tests';
+    public static int $views = 0;
+    public static string $date = '2020-04-20T18:00:00';
+
+    public static function setUpBeforeClass(): void
+    {
+        $uuid = self::$uuid;
+        $author_name = new AuthorName(self::$author_name);
+        $image = self::$image;
+        $slug = new Slug(self::$slug);
+        $content = self::$content;
+        $views = self::$views;
+        $now = new DateTime(self::$date);
+        $created = $now;
+        $updated = $now;
+        self::$post = new Post($uuid, $author_name, $slug, $image, $content, $views, $created, $updated);
+    }
+
+    public function testCreateNewPostWithErrors()
+    {
+        $post = Post::createNewPost('wrong name', self::$slug, self::$image, self::$content);
+        $this->assertContains('fail', $post);
+        $this->assertSame('The author name cannot have special characters.', $post['data']['authorName']);
+
+        $post = Post::createNewPost(self::$author_name, 'wrong slug', self::$image, self::$content);
+        $this->assertContains('fail', $post);
+        $this->assertSame('The slug is not in the correct format.', $post['data']['slug']);
+    }
+
+    public function testCreateANewPostWithRightData()
+    {
+        $post = Post::createNewPost(self::$author_name, self::$slug, self::$image, self::$content);
+        $this->assertSame('app\Domain\Post', get_class($post));
+    }
+
+    public function testSaveANewPostOnDB()
+    {
+        $post = Post::createNewPost(self::$author_name, self::$slug, self::$image, self::$content);
+        $saved = $post->savePost();
+        $this->assertContains('success', $saved);
+        $this->assertSame('Post saved successfully.', $saved['data']['post']);
+    }
+
+    public function testSaveSamePostAgain()
+    {
+        $post = Post::createNewPost(self::$author_name, self::$slug, self::$image, self::$content);
+        $post->savePost(); //first try to get data on DB
+        $saved = $post->savePost();
+        $this->assertContains('fail', $saved);
+        $this->assertSame('Error, there is already a post with this slug.', $saved['data']['slug']);
+    }
+
+    public function testUpdateAuthorWithWrongAuthorName()
+    {
+        $post = Post::createNewPost(self::$author_name, self::$slug, self::$image, self::$content);
+        $post->savePost();
+        $update_author = $post->updateAuthor('wrong author');
+        $this->assertContains('fail', $update_author);
+    }
+
+    public function testUpdateAuthroWithRightAuthorName()
+    {
+        $post = Post::createNewPost(self::$author_name, self::$slug, self::$image, self::$content);
+        $update_author = $post->updateAuthor('newAuthor1');
+        $this->assertContains('success', $update_author);
+        $this->assertSame('Author name updated successfully.', $update_author['data']['authorName']);
+    }
+
+    public function testInsertionOnPrivateProperties()
+    {
+        $this->expectErrorMessage('Cannot access private property app\Domain\Post::$uuid');
+        self::$post->uuid = '12312312312';
+    }
+
+    public function testGetProperties()
+    {
+        $post = self::$post;
+
+        $this->assertSame('app\Domain\Post', get_class($post));
+        $this->assertSame($post->uuid, self::$uuid);
+        $this->assertSame((string) $post->author_name, self::$author_name);
+        $this->assertSame((string) $post->slug, self::$slug);
+        $this->assertSame($post->image, self::$image);
+        $this->assertSame($post->content, self::$content);
+        $this->assertSame($post->views, self::$views);
+        $date = new DateTime(self::$date);
+        $this->assertSame($post->created->format('Y-m-d'), $date->format('Y-m-d'));
+    }
+
+    protected function tearDown(): void
+    {
+        Db::connection('mongodb')->collection('test')->where('slug', self::$slug)->delete();
+    }
+}
