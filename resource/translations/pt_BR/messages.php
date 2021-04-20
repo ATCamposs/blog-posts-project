84afef1eda40e36122b9b06d4143282c90cbd956
ATCamposs <andre.tatibano@gmail.com>
2021-04-20 19:49:56 -0300

    Adicionadas traduções para o dominio principal

--- resource/translations/pt_BR/messages.php
+++ resource/translations/pt_BR/messages.php
@@ -2,13 +2,22 @@
 
 return [
     //ValueObject/AuthorName.php
-    'The author name cannot be empty.' => 'O nome do autor não pode estar vazio',
-    'The author name must be at least 3 characters.' => 'O nome do autor deve ter pelo menos 3 caracteres',
-    'The author name must be less than 26 characters.' => 'O nome do autor deve ter menos de 26 caracteres',
-    'The author name cannot have special characters.' => 'O nome do autor não pode ter caracteres especiais',
+    'The author name cannot be empty.' => 'O nome do autor não pode estar vazio.',
+    'The author name must be at least 3 characters.' => 'O nome do autor deve ter pelo menos 3 caracteres.',
+    'The author name must be less than 26 characters.' => 'O nome do autor deve ter menos de 26 caracteres.',
+    'The author name cannot have special characters.' => 'O nome do autor não pode ter caracteres especiais.',
 
     //ValueObject/Slug.php
-    'The slug cannot be empty.' => 'O slug não pode estar vazio',
-    'The slug must be at least 8 characters.' => 'O slug deve ter pelo menos 8 caracteres',
-    'The slug is not in the correct format.' => 'O slug está no formato incorreto'
+    'The slug cannot be empty.' => 'O slug não pode estar vazio.',
+    'The slug must be at least 8 characters.' => 'O slug deve ter pelo menos 8 caracteres.',
+    'The slug is not in the correct format.' => 'O slug está no formato incorreto.',
+
+    //Domain/Post.php
+    'Author name updated successfully.' => 'Nome do autor atualizado com sucesso.',
+    'Error, there is already a post with this slug.' => 'Erro, já existe um post com este slug.',
+    'Post saved successfully.' => 'Post salvo com sucesso.',
+    'The post could not be found' => 'A postagem não pode ser encontrada',
+
+    //Generic Error
+    'Error, please try again.' => 'Erro, por favor tente novamente.'
 ];
