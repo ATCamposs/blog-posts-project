0bd525c25f98fda4dbf1bff8871a9b40301a98d5
ATCamposs <andre.tatibano@gmail.com>
2021-04-20 12:48:34 -0300

    Adição imagem e quantidade views ao post, e método get para valores.

--- app/Domain/Post.php
+++ app/Domain/Post.php
@@ -11,24 +11,39 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class Post extends Model
 {
+    use PostTrait;
+
     private string $uuid;
     private AuthorName $author_name;
     private Slug $slug;
+    private string $image;
     private string $content;
+    private int $views;
     private DateTime $created;
     private DateTime $updated;
 
     public function __construct(
         string $uuid,
         AuthorName $author_name,
+        Slug $slug,
+        string $image,
         string $content,
+        int $views,
         DateTime $created,
         DateTime $updated
     ) {
         $this->uuid = $uuid;
         $this->author_name = $author_name;
+        $this->slug = $slug;
+        $this->image = $image;
         $this->content = $content;
+        $this->views = $views;
         $this->created = $created;
         $this->updated = $updated;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
     }
 }
