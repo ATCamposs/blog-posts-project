de81f4bc47035920dc20dfe9c5a5cdf63dde6189
ATCamposs <andre.tatibano@gmail.com>
2021-04-19 21:28:41 -0300

    Adicionado UUID ao post

--- app/Domain/Post.php
+++ app/Domain/Post.php
@@ -11,14 +11,21 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class Post extends Model
 {
+    private string $uuid;
     private AuthorName $author_name;
     private Slug $slug;
     private string $content;
     private DateTime $created;
     private DateTime $updated;
 
-    public function __construct(AuthorName $author_name, string $content, DateTime $created, DateTime $updated)
-    {
+    public function __construct(
+        string $uuid,
+        AuthorName $author_name,
+        string $content,
+        DateTime $created,
+        DateTime $updated
+    ) {
+        $this->uuid = $uuid;
         $this->author_name = $author_name;
         $this->content = $content;
         $this->created = $created;
